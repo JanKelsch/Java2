795ade473730946c00a4f2d7749b04fe56f89c92
JanKelsch <49919648+JanKelsch@users.noreply.github.com>
2019-07-10 21:25:35 +0200

    Update Person.java

--- 2019-06-26-HW2-Stream/src/Person.java
+++ 2019-06-26-HW2-Stream/src/Person.java
@@ -1,1 +1,28 @@
+import java.util.List;
 
+public class Person {
+	private String name;
+	private List<String> bankAccounts;
+	
+	public Person(String name, List<String> bankAccounts) {
+		this.name = name;
+		this.bankAccounts = bankAccounts;
+	}
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<String> getBankAccounts() {
+		return bankAccounts;
+	}
+}
