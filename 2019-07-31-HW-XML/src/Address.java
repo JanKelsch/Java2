1705cb3ce75175a9a4f89159e1b26afc1a082c44
JanKelsch <kelsh@bigmir.net>
2019-08-27 14:20:51 +0200

    add code from xml to object

--- 2019-07-31-HW-XML/src/Address.java
+++ 2019-07-31-HW-XML/src/Address.java
@@ -6,20 +6,4 @@ public class Address {
         this.street = street;
         this.houseNumber = houseNumber;
     }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public int getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(int houseNumber) {
-        this.houseNumber = houseNumber;
-    }
 }
--- 2019-07-31-HW-XML/src/Person.java
+++ 2019-07-31-HW-XML/src/Person.java
@@ -8,28 +8,4 @@ public class Person {
         this.gender = gender;
         this.address = address;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
